cabf7e46ca2c1c2f3b7c5accc8ed1dba4af03c0c
Jeremy Hall <jer@jersrealm.com>
2021-10-10 03:37:58 -0500

    assignment 2 started, instructions issues again. FULL STOP

---
+++ fun_with_collections/sort_and_search_lists.py
@@ -0,0 +1,43 @@
+"""
+Program: basic_lists.py
+Author: Jeremy Hall
+Last date modified: 10/9/2021
+
+The purpose of this program is to pass a number into a function and then that function calls a second
+function and then passes the input back to the calling function.
+"""
+
+def sort_list():
+    """
+    i was instructed to add this docstring to the function.  I don't know what it is and I dont believe
+    its been covered or even mentioned up to this point.
+
+    use reST style.
+    :param parameter_1: this is what the first parameter represents
+    :param parameter_2: this is what the second parameter represents
+    :returns: this is what is returned
+    :raises key/Error: raises an exception
+    """
+
+    #needs to return the same object it was passed if sorting it creates a new object (use ID)
+
+pass
+
+
+
+def search_list():
+    """
+    i was instructed to add this docstring to the function.  I don't know what it is and I dont believe
+    its been covered or even mentioned up to this point.
+
+    use reST style.
+    :param parameter_1: this is what the first parameter represents
+    :param parameter_2: this is what the second parameter represents
+    :returns: this is what is returned
+    :raises key/Error: raises an exception
+    """
+pass
+
+if __name__ =='__main__':      #again this file is not MAIN.  I still/again don't understand this.
+
+pass
